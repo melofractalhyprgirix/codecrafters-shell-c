9fe556edd48e434b3abfd94afe92f0f6803a8a78
DevProDelta <m3l0fractalhyprgirix@proton.me>
2026-09-18 13:59:09 -0700

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+  char userInput[50];
   // Flush after every printf
   setbuf(stdout, NULL);
-
   printf("$ ");
-
+  fgets(userInput, sizeof(userInput), stdin);
+  userInput[strlen(userInput)-1] = '\0';
+  printf("%s: command not found", userInput);
   return 0;
 }
