4be96795c4d5fcf2331466d869a7e80584a43f43
DevProDelta <m3l0fractalhyprgirix@proton.me>
2026-09-18 14:10:33 -0700

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[]) {
   char userInput[50];
-  // Flush after every printf
+  bool inTerminal = true;
+  while(inTerminal) {
+  //Flush after every printf
   setbuf(stdout, NULL);
   printf("$ ");
   fgets(userInput, sizeof(userInput), stdin);
   userInput[strlen(userInput)-1] = '\0';
-  printf("%s: command not found", userInput);
+  printf("%s: command not found\n", userInput);
+  }
   return 0;
 }
